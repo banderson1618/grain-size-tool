9ff9296fd5437ba508c378ea613f2b08e6918d84
Braden Anderson <A02150284@aggies.usu.edu>
2017-03-31 17:39:10 -0600

    Initial Commit

---
+++ GrainSize.py
@@ -0,0 +1,23 @@
+"""
+
+Name: Grain Size Distribution Tool
+Purpose: Find the average size of gravel in a stream network
+
+Author: Braden Anderson
+Created: 31 March 2017
+Last Update: 31 March 2017
+
+"""
+
+import arcpy
+
+
+
+def main(dem, streamNetwork):
+    """Source code for our tool"""
+    arcpy.env.overwriteOutput = True
+
+
+if __name__ == '__main__':
+    main(sys.argv[1],
+         sys.argv[2])
