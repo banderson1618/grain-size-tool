b5a78f7d245cf5ea0dc0f5a24986bd183b1f2eec
Braden Anderson <A02150284@aggies.usu.edu>
2017-04-07 17:22:30 -0600

    Wrote the findSlope() function and tested it

--- GrainSize.py
+++ GrainSize.py
@@ -23,20 +23,23 @@ def main(dem,
     arcpy.env.overwriteOutput = True
     arcpy.CheckOutExtension("Spatial")
 
+    testing = False
+
     if not os.path.exists(scratch+"\outputData"):
         os.makedirs(scratch+"\outputData")
-    clippedStreamNetwork = scratch + "\outputData\clippedStreamNetwork.shp"
+    scratch += "\outputData"
+    clippedStreamNetwork = scratch + "\clippedStreamNetwork.shp"
     arcpy.AddMessage("Clipping stream network...")
     arcpy.Clip_analysis(streamNetwork, huc10, clippedStreamNetwork)
 
-    reachArray = makeReaches(dem, clippedStreamNetwork, precipMap)
+    reachArray = makeReaches(testing, dem, clippedStreamNetwork, precipMap, scratch)
+    for i in range(1, 2500, 100):
+        arcpy.AddMessage("Slope: " + str(reachArray[i].slope))
     arcpy.AddMessage("Reach Array Created.")
 
-    for i in range(10):
-        arcpy.AddMessage(str(reachArray[i].xyPosition))
 
 
-def makeReaches(dem, streamNetwork, precipMap):
+def makeReaches(testing, dem, streamNetwork, precipMap, scratch):
     """Creates a series of reaches """
     # TODO: Fix the reach's xy coordinates being wrong
     """This commented out code was used to make sure that the layer's data itself wasn't being messed up
@@ -55,13 +58,30 @@ def makeReaches(dem, streamNetwork, precipMap):
     flowAccumulation = arcpy.sa.FlowAccumulation(flowDirection)  # Calculates the flow accumulation to use in findWidth()
 
     arcpy.AddMessage("Creating Reach Array...")
-    for polyline in polylineCursor:
+
+    """If testing, only go through the loop once. Otherwise, go through every reach"""
+    if testing:
+        polyline = polylineCursor.next()
         width = findWidth(flowAccumulation, precipMap, polyline[0].firstPoint)
         q_2 = findQ_2()
-        slope = findSlope()
-        reach = Reach(width, q_2, polyline[0], polyline[1])
+        slope = findSlope(dem, polyline, scratch)
+
+        reach = Reach(width, q_2, slope, polyline[0])
 
         reaches.append(reach)
+
+    else:
+        for polyline in polylineCursor:
+            width = findWidth(flowAccumulation, precipMap, polyline[0].firstPoint)
+            q_2 = findQ_2()
+            slope = findSlope(dem, polyline, scratch)
+
+            reach = Reach(width, q_2, slope, polyline[0])
+
+            reaches.append(reach)
+
+    del polyline
+    del polylineCursor
 
     return reaches
 
@@ -79,10 +99,43 @@ def findWidth(dem, precipMap, point):
     i = 1  # placeholder
     return i
 
-def findSlope(dem, polyline):
+def findSlope(dem, polyline, scratch):
     """Finds the average slope of the reach in question"""
-    length = polyline.length
-    # startingElevation =
+    length = polyline[0].length
+    firstPointElevation = findElevationAtPoint(dem, polyline[0].firstPoint,scratch)
+    secondPointElevation = findElevationAtPoint(dem, polyline[0].lastPoint, scratch)
+
+    elevationDifference = abs(firstPointElevation - secondPointElevation)
+
+    slope = (elevationDifference/length) * -1
+
+    return slope
+
+
+def findElevationAtPoint(dem, point, scratch):
+    """Finds the elevation at a certain point based on a DEM"""
+    """
+    I can't find a good way to just pull the data straight from the raster, so instead, we're having to
+    create the point in a layer of its own, then create another layer that has the elevation using the Extract Value
+    to Points tool, then using a search cursor to get the elevation data. It's a mess, and it's inefficient, but it
+    works. If anyone can find a better way, email me at banderson1618@gmail.com
+    """
+    sr = arcpy.Describe(dem).spatialReference
+    arcpy.env.workspace = scratch
+    arcpy.CreateFeatureclass_management(scratch, "point.shp", "POINT", "", "DISABLED", "DISABLED", sr)
+    fc = scratch+"\point.shp"
+    cursor = arcpy.da.InsertCursor(fc, ["SHAPE@"])
+    cursor.insertRow([point])
+    del cursor
+    pointLayer = scratch+"\pointElevation"
+    arcpy.sa.ExtractValuesToPoints(scratch+"\point.shp", dem, pointLayer)
+    searchCursor = arcpy.da.SearchCursor(pointLayer+".shp", "RASTERVALU")
+    row = searchCursor.next()
+    elevation = row[0]
+    del searchCursor
+    del row
+
+    return elevation
 
 
 if __name__ == '__main__':
--- Reach.py
+++ Reach.py
@@ -1,14 +1,14 @@
 class Reach(object):
-    def __init__(self, width, q_2, polyline, xyPosition):
+    def __init__(self, width, q_2, slope, polyline):
         """
-        :type length: length of the given polyline
-        :type xyPosition: The x y position of the centroid of the polyline
         :type width: The average width of the reach
         :type q_2: The flow at a 2 year flood
+        :type slope: The average slope of the reach
         :type polyline: The complete polyline
+        :type xyPosition: The x y position of the centroid of the polyline
         """
-        self.length = polyline.length
-        self.xyPosition = xyPosition
         self.width = width
         self.q_2 = q_2
+        self.slope = slope
         self.polyline = polyline
+        self.xyPosition = polyline.centroid
